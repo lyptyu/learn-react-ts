8ae422913aa1c2883d0b6e2b2677ca956d3c9650
林怡鹏 <linyipeng@limayao.com>
2022-06-30 15:04:27 +0800

    109

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import './01-ts基础/01-基本类型'
 ReactDOM.render(
   // <React.StrictMode>
     <App />,
